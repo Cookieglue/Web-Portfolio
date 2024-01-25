c30cfc20ac29d90fc2b396e310a3ad380133eb6b
mari <63924379+Cookieglue@users.noreply.github.com>
2024-01-25 12:29:55 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Get the div element by its ID
+var myButton = document.getElementById('myButton');
+
+// Add a click event listener
+myButton.addEventListener('click', function() {
+  alert('Button clicked!');
+  // Add any other functionality you want the button to perform
+});
